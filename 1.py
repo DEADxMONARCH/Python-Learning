e53b540f4c38ef7e141220a4cb24fe89cfa1eda9
DEADxMONARCH <vaibhav2005lucky@gmail.com>
2025-11-30 14:15:56 +0530

    step1

---
+++ 1.py
@@ -0,0 +1,14 @@
+print("hello world")
+# hello bhai
+# bhai 
+# luo
+print('''Dil mera hai nasamajh kitna
+Besabar ye bewkoof bada
+Chahta hai kitna tujhe
+Khud magar nahi jaan sakaa''')
+# a = 1
+# b = 2
+# print(a+b)
+a = int(input("enter a : "))
+b = int(input("enter b : "))
+print(a+b)
